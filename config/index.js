3964d5cb5121fc19d2bed2f13b8604190fde0fee
hhjyhxw <512162086@qq.com>
2020-05-21 23:47:41 +0800

    init

---
+++ config/index.js
@@ -0,0 +1,12 @@
+const dev = {
+	// baseUrl: 'http://127.0.0.1:8080',
+	// baseUrl: 'http://192.168.8.188:8081',
+	baseUrl: 'https://www.szxmyf.com.cn/laimaiapi/api/',
+	// baseUrl: 'https://demo.easycampus.cn',
+	h5Appid: 'wxb66b599f7f61b46f',
+	debug: true
+}
+
+export function def() {
+	return dev
+}
